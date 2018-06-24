3ecbbbc67a0e6d9779d506f694dcdceaf380edb2
Alexandra <35684952+Vortexlash@users.noreply.github.com>
2018-06-24 19:25:11 +0400

    Added cascading conditional statements :alien:

--- xcom ops name generator.py
+++ xcom ops name generator.py
@@ -13,12 +13,14 @@ opname = (fw[rfn]) + (lw[rln])
 #missiontypes
 
 rmt = random.randrange(0, 7)
-mt = ["Alien Abductions", "Alien Base Assault", "Council Mission", "Temple Ship Assault", "Alien Terror Attack", "UFO Crash Site", "UFO Landing Site"]
+mt = ["Alien Abductions", "Alien Base Assault", "Council Mission", "Temple Ship Assault", "Alien Terror Attack", "UFO Crash Site", "UFO Landing Site", "XCOM Base Defense"]
 
 optype = (mt[rmt])
 
 if rmt == 3:
     opname = "Avenger"
+elif rmt == 7:
+    opname = "Ashes And Temples"
     
 print("Operation " + '"' + (opname) + '"')
 
@@ -29,11 +31,118 @@ print(optype)
 rml = random.randrange(0, 15)
 cl = ["USA", "Canada", "France", "United Kingdom", "Germany", "Nigeria", "Egypt", "South Africa", "China", "Japan", "India", "Australia", "Brazil", "Argentina", "Mexico", "Russia"]
 
-rpl = random.randrange(0, 4) #paniclevel
-
 oplocation = (cl[rml])
 
 print(oplocation)
+
+#missionobjectives
+
+rcm = random.randrange(0, 24)
+cm = ["Asset Recovery [1]", "Asset Recovery [2]", "Asset Recovery [3]", "Asset Recovery [4]", "Asset Recovery [5]", "Asset Recovery [6]", "Bomb Disposal [1]", "Bomb Disposal [2]", "Bomb Disposal [3]", "Bomb Disposal [4]", "Bomb Disposal [5]", "Target Escort [1]", "Target Escort [2]", "Target Escort [3]", "Target Extraction [1]", "Target Extraction [2]", "Target Extraction [3]", "Site Recon", "Portent", "Deluge", "Furies", "Friends in Low Places", "Confounding Light", "Gangplank", "Gateway"]
+
+opbrief = None
+optasks = None
+
+if rmt == 0:
+    opbrief = "Alien abduction in progress. Site is clear of civilians; collateral damage is not a concern."
+    optasks = "Neutralize all hostile targets. Locate and secure any Meld canisters."
+elif rmt == 1:
+    opbrief = "We have gained access to an Alien Base by means of the Skeleton Key. Use extreme caution; analysis indicates the presence of a large combined enemy contingent. This mission cannot be aborted."
+    optasks = "Penetrate the base's defenses. Neutralize all invader forces. Identify and capture the base commander, if possible."
+elif rmt == 2:
+    if rcm == 0:
+        opbrief = "Alien forces have infiltrated the site and will actively oppose our efforts."
+        optasks = "Sweep the area of operations. Eliminate all opposition."
+    elif rcm == 1:
+        opbrief = "Alien forces have infiltrated the site and will actively oppose our efforts."
+        optasks = "Sweep the area of operations. Eliminate all opposition."
+    elif rcm == 2:
+        opbrief = "Alien forces have infiltrated the site and will actively oppose our efforts."
+        optasks = "Sweep the area of operations. Eliminate all opposition."
+    elif rcm == 3:
+        opbrief = "Alien forces have infiltrated the site and will actively oppose our efforts."
+        optasks = "Sweep the area of operations. Eliminate all opposition."
+    elif rcm == 4:
+        opbrief = "Alien forces have infiltrated the site and will actively oppose our efforts."
+        optasks = "Sweep the area of operations. Eliminate all opposition."
+    elif rcm == 5:
+        opbrief = "Alien forces have infiltrated the site and will actively oppose our efforts."
+        optasks = "Sweep the area of operations. Eliminate all opposition."
+    elif rcm == 6:
+        opbrief = "The invaders have placed a bomb in the area of operations to inflict maximum destruction and panic."
+        optasks = "Deactivate the bomb before it goes off. Delay the detonation countdown by deactivating the bomb's power sources. Prevent reactivation by eliminating all remaining opposition."
+    elif rcm == 7:
+        opbrief = "The invaders have placed a bomb in the area of operations to inflict maximum destruction and panic."
+        optasks = "Deactivate the bomb before it goes off. Delay the detonation countdown by deactivating the bomb's power sources. Prevent reactivation by eliminating all remaining opposition."
+    elif rcm == 8:
+        opbrief = "The invaders have placed a bomb in the area of operations to inflict maximum destruction and panic."
+        optasks = "Deactivate the bomb before it goes off. Delay the detonation countdown by deactivating the bomb's power sources. Prevent reactivation by eliminating all remaining opposition."
+    elif rcm == 9:
+        opbrief = "The invaders have placed a bomb in the area of operations to inflict maximum destruction and panic."
+        optasks = "Deactivate the bomb before it goes off. Delay the detonation countdown by deactivating the bomb's power sources. Prevent reactivation by eliminating all remaining opposition."
+    elif rcm == 10:
+        opbrief = "The invaders have placed a bomb in the area of operations to inflict maximum destruction and panic."
+        optasks = "Deactivate the bomb before it goes off. Delay the detonation countdown by deactivating the bomb's power sources. Prevent reactivation by eliminating all remaining opposition."
+    elif rcm == 11:
+        opbrief = "A high value target requires an escort for evacuation to XCOM HQ. However, hostile units in the area are on high alert, and will attempt to counter our extraction team."
+        optasks = "Escort the target safely to the extraction point. Protect the target at all costs."
+    elif rcm == 12:
+        opbrief = "A high value target is in position for evacuation to XCOM HQ. However, hostile units in the area are on high alert, and will attempt to counter our extraction team."
+        optasks = "Escort the target to the extraction point. Protect the target at all costs."
+    elif rcm == 13:
+        opbrief = "A high value target is in position for evacuation to XCOM HQ. However, hostile units in the area are on high alert, and will attempt to counter our extraction team."
+        optasks = "Escort the target to the extraction point. Protect the target at all costs."
+    elif rcm == 14:
+        opbrief = "A high value target requires an escort for evacuation to XCOM HQ. However, hostile units in the area are on high alert, and will attempt to counter our extraction team."
+        optasks = "Find the Target. Escort the target safely to the extraction point. Protect the target at all costs."
+    elif rcm == 15:
+        opbrief = "A high value target requires an escort for evacuation to XCOM HQ. However, hostile units in the area are on high alert, and will attempt to counter our extraction team."
+        optasks = "Find the target. Escort the target safely to the extraction point. Protect the target at all costs."
+    elif rcm == 16:
+        opbrief = "Intel indicates a high value civilian asset somewhere at site; exact location unknown. The enemy is present in unknown numbers."
+        optasks = "Locate the VIP. Protect the VIP at all costs."
+    elif rcm == 17:
+        opbrief = "The fishing village still shows no signs of life. The nature of the aliens' involvement is unknown, as is the type and level of resistance. Extreme caution is warranted."
+        optasks = "Investigate the site. Identify and eliminate all opposition."
+    elif rcm == 18:
+        opbrief = "A French military convoy was caught in an ambush. The convoy's current status is unknown. Be prepared for anything."
+        optasks = "Investigate the area of operations. Locate and secure any survivors as well as the convoy's cargo. Eliminate any remaining opposition."
+    elif rcm == 19:
+        opbrief = "The transport is stopped on a dam in France; it's likely the aliens have sabotaged the structure. They clearly consider this transport to be high value. Expect substantial opposition."
+        optasks = "Secure the dam by any means necessary. Locate and secure the convoy and its contents."
+    elif rcm == 20:
+        opbrief = "The aliens are holding Annette's fellow abductees on their ship and will not easily let them go. Secure the captives as quickly as possible."
+        optasks = "Locate and rescue the other abductees. Eliminate all opposition."
+    elif rcm == 21:
+        opbrief = "If the contact's device is genuine, as we suspect it is, it'll be of high value. Stay sharp, as the aliens may make an effort to recover it."
+        optasks = "Escort VIP and the alien artifact to the extraction point. Eliminate any resistance. VIP must survive."
+    elif rcm == 22:
+        opbrief = "A battleship is closing on the city. The squad must place the transponders in time to divert the attack. Aliens will likely deploy scouts to seek out any human resistance. Enemy contact likely."
+        optasks = "Plant the transponders on the train. Once the transponders are in place, activate the train's drive control system."
+    elif rcm == 23:
+        opbrief = "The battleship is crippled but still active, and the aliens aboard won't be happy to see us. Expect maximum resistance."
+        optasks = "Destroying or disabling the power conduits will enable us to bring down the Battleship without damaging it. Eliminate all alien defenders."
+    elif rcm == 24:
+        opbrief = "Alien forces are in the area; witnesses report seeing multiple canisters of unknown purpose. Civilians have been evacuated."
+        optasks = "Neutralize all hostile targets. Locate and secure the canisters for analysis at HQ."
+elif rmt == 3:
+    opbrief = "We finally have the strength to face the Ethereals on their own ship. The future of humanity is at stake. Expect extreme resistance."
+    optasks = "Force entry to the Temple Ship's bridge. The Volunteer must survive."
+elif rmt == 4:
+    opbrief = "Alien terror attack in progress. Strong civilian presence; every civilian casualty will sharply increase panic in this country. Failure will have severe political consequences for XCOM."
+    optasks = "Approach civilians to enable evacuations. Minimize civilian casualties. Neutralize all hostile targets"
+elif rmt == 5:
+    opbrief = "XCOM Interceptors have splashed a UFO. Intel indicates some surviving crew. Exercise caution when engaging the enemy; the craft may contain usable salvage."
+    optasks = "Locate the crashed UFO. Sweep the area for surviving crew and neutralize them. Locate and secure any Meld canisters. Avoid additional damage to the craft if possible; some components may be recoverable."
+elif rmt == 6:
+    opbrief = "Satellite data indicates a UFO has touched down; high probability of enemy operation in progress. Expect to face a full complement of enemy crew."
+    optasks = "Gain entry to the UFO and neutralize all enemy forces. Locate and secure any Meld canisters."
+elif rmt == 7:
+    opbrief = "The invaders have infiltrated XCOM in unknown numbers. Limited reinforcements, including XCOM Base Security personnel, are available. Failure will mean a massive setback for XCOM."
+    optasks = "Defend our base by all necessary means. If all soldiers in the combat area are killed, XCOM HQ will suffer massive damage and loss of resources."
+
+print("MISSION BRIEF: " + (opbrief))
+print("MISSION OBJECTIVES: " + (optasks))
 
 #tacticaltips
 
